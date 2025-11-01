e34176b3ed7dd49da67657ce97f1e3d2b21deaf3
Charlie Brown Js <celiokn@gmail.com>
2025-11-01 13:20:10 -0300

    update security and add app comentary

--- App.tsx
+++ App.tsx
@@ -1,11 +1,8 @@
-// comentários sempre em pt-br
-
 import React, { useState } from 'react';
 import { LoginPage } from './components/LoginPage';
 import { HomePage } from './components/HomePage';
 import type { User } from './types';
 
-// Componente principal do App
 function App() {
   const [user, setUser] = useState<User | null>(null);
 
@@ -13,12 +10,10 @@ function App() {
     setUser(loggedInUser);
   };
 
-  // Função para lidar com o logout
   const handleLogout = () => {
     setUser(null);
   };
 
-  // Renderiza o componente apropriado com base no estado de autenticação do usuário
   return (
     <div className="antialiased font-sans">
       {user ? (
