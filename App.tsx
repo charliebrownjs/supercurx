eb0e73625a6c98eef0e52b2da524e6c74edaf192
Charliebrownjs <celiokn@gmail.com>
2025-11-01 13:17:33 -0300

    app.tsx update comments to pt-br

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
+// comentários sempre em pt-br
+
 import React, { useState } from 'react';
 import { LoginPage } from './components/LoginPage';
 import { HomePage } from './components/HomePage';
 import type { User } from './types';
 
+// Componente principal do App
 function App() {
   const [user, setUser] = useState<User | null>(null);
 
@@ -10,10 +13,12 @@ function App() {
     setUser(loggedInUser);
   };
 
+  // Função para lidar com o logout
   const handleLogout = () => {
     setUser(null);
   };
 
+  // Renderiza o componente apropriado com base no estado de autenticação do usuário
   return (
     <div className="antialiased font-sans">
       {user ? (
